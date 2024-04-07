980aff333ddb5668fd2d93410426472adb320b20
furtidev <midrashahil123@gmail.com>
2024-04-08 00:46:11 +0600

    🎉ready, set, go!

---
+++ generate_data.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+type Device struct {
+	ProductType string
+	VendorName  string
+	ProductName string
+	ProductId   string
+	VendorId    string
+	Speed string
+	Version string
+}
+
+type Data struct {
+	Devices []Device
+}
+
+var alreadyVisitedIds []string
+
+// stolen from: https://stackoverflow.com/a/75049947
+func getAbs(path string, locationPath string) string {
+	new_file_path := path
+	new_abs_path := path
+	if filepath.IsAbs(new_file_path) {
+		new_abs_path = new_file_path
+	} else {
+		new_abs_path, _ = filepath.Abs(filepath.Join(locationPath, path))
+	}
+	return new_abs_path
+}
+
+func getBasicValues(location string) string {
+	var result string
+	value, err := os.ReadFile(location)
+	if err != nil {
+		result = "unknown"
+	} else {
+		result = string(value)[:len(value)-1]
+	}
+	return result
+}
+
+func genData() Data {
+	usb_interface := "/sys/bus/usb/devices"
+	data := Data{}
+	filepath.WalkDir(usb_interface, func(path string, d fs.DirEntry, err error) error {
+		if path != usb_interface {
+			if parts := strings.Split(path, ":"); len(parts) < 2 {
+				walkFolder(&data, path)
+			}
+		}
+		return nil
+	})
+	return data
+}
+
+func walkFolder(data *Data, location string) {
+	linkPath, _ := os.Readlink(location)
+	location = getAbs(linkPath, "/sys/bus/usb/devices")
+	filepath.WalkDir(location, func(path string, d fs.DirEntry, err error) error {
+		if d.Name() == "idProduct" {
+			dev := Device{}
+			dev.ProductId = getBasicValues(path)
+			if slices.Contains(alreadyVisitedIds, dev.ProductId) {
+				return nil
+			}
+
+			dev.VendorId = getBasicValues(location + "/idVendor")
+
+			dev.ProductType = getBasicValues(location + "/product")
+
+			dev.Speed = getBasicValues(location + "/speed")
+
+			dev.Version = getBasicValues(location + "/version")
+
+			// nasty consequences of using interfaces
+			if x, ok := Ids[dev.VendorId].(map[string]interface{}); ok {
+				dev.VendorName = x["name"].(string)
+				devices := x["devices"].(map[string]string)
+				name, ok := devices[dev.ProductId]
+				if !ok {
+					name = "unknown"
+				}
+				dev.ProductName = name
+			} else {
+				dev.VendorName, dev.ProductName = "unknown", "unknown"
+			}
+			data.Devices = append(data.Devices, dev)
+			alreadyVisitedIds = append(alreadyVisitedIds, dev.ProductId)
+		}
+		return nil
+	})
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"runtime"
+)
+
+func main() {
+	green := color.New(color.FgGreen).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintFunc()
+	red := color.New(color.FgHiRed).SprintFunc()
+	if runtime.GOOS == "linux" {
+		data := genData()
+		for _, v := range data.Devices {
+			var deviceType string = v.ProductType
+			var productName string = "("+v.ProductName+")"
+			if deviceType == "unknown" && v.ProductName != "unknown" {
+				deviceType = v.ProductName
+			}
+			if v.ProductName == "unknown" {
+				productName = ""
+			}
+			fmt.Printf("%s\n    Vendor ID   %s %s\n    Product ID  %s %s\n    Speed       %s\n    Version    %s\n", green(deviceType), yellow(v.VendorId), cyan(v.VendorName), yellow(v.ProductId), cyan(productName), yellow(v.Speed+"Mbit/s"), yellow(v.Version))
+		}
+	} else {
+		fmt.Printf("%s is %s a supported platform.\n", runtime.GOOS, red("not"))
+	}
+}
